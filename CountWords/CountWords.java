96fb81f77bb0e91f6b09f041f1f63803b074a6c4
wachtels <steffen.wachtel@hs-furtwangen.de>
2016-05-04 11:20:13 +0200

    Kommentar entfernt
    beschreibung

--- CountWords/CountWords.java
+++ CountWords/CountWords.java
@@ -4,7 +4,7 @@ import java.io.File;
 //import java.util.HashMap;
 import java.util.Scanner;
 import java.util.TreeMap;
-// Testkommentar fuer Git
+
 public class CountWords {
 	
 	   public static void main( String args[]) {
